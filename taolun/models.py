d923debec5da2e0505bf299ba24426b39045d189
meizongqi <398007089@qq.com>
2020-11-04 16:20:57 +0800

    user模型类匹配

--- taolun/models.py
+++ taolun/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,7 +6,7 @@ from django.db import models
 
 # 讨论组
 class Groups(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name="讨论组名称")
+    name = models.CharField(max_length=255, unique=True, verbose_name="讨论组的名称")
 
     class Meta:
         verbose_name = "讨论组"  # 在admin站点显示名称
@@ -36,7 +37,7 @@ class Posting(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True, verbose_name="发布日期")
     update_date = models.DateTimeField(auto_now=True, verbose_name="更新日期")
     isdelete = models.IntegerField(default=0, verbose_name="是否被删除(逻辑删除)")
-    user = models.ForeignKey("users.User", related_name="posting_user",
+    user = models.ForeignKey(User, related_name="posting_user",
                              verbose_name="用户", on_delete=models.CASCADE)
     topics = models.ManyToManyField("Topics", verbose_name='所属话题')
 
@@ -53,7 +54,7 @@ class Comment(models.Model):
     comment = models.TextField(max_length=1000, verbose_name="内容")
     articles = models.ForeignKey('Posting', related_name='posting_comment',
                                  on_delete=models.CASCADE, verbose_name="评论的帖子")
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, verbose_name="评论用户")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="评论用户")
     publish_date = models.DateTimeField(auto_now=True, verbose_name="评论日期")
     parent = models.ForeignKey("self", related_name='parent_comment',
                                blank=True, null=True, on_delete=models.CASCADE)
