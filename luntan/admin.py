36ff0991986efc90bfcb3eed7efd4b5f5aa8f8ef
meizongqi <398007089@qq.com>
2020-11-10 11:19:29 +0800

    论坛

--- luntan/admin.py
+++ luntan/admin.py
@@ -3,13 +3,13 @@ from . import models
 
 
 # Register your models here.
-class CommentInline(admin.TabularInline):
-    model = models.Comment
+# class CommentInline(admin.TabularInline):
+    # model = models.Comment
 
 
-class ArticlesAdmin(admin.ModelAdmin):
-    inlines = [CommentInline]  # Inline
+# class ArticlesAdmin(admin.ModelAdmin):
+#     inlines = [CommentInline]  # Inline
 
 
-admin.site.register(models.Articles, ArticlesAdmin)
-admin.site.register([models.Areas, models.ThumbUp, models.Comment, models.Topics])
+# admin.site.register(models.Articles, ArticlesAdmin)
+admin.site.register([models.Areas, models.Articles, models.ThumbUp, models.Comment, models.Topics])
