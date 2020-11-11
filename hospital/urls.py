e56720fae07706c790e365c9f95f54a550b2076b
meizongqi <398007089@qq.com>
2020-11-11 10:37:02 +0800

    医院

--- hospital/urls.py
+++ hospital/urls.py
@@ -5,7 +5,7 @@ from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
-    path(r'index/', views.Index.as_view(), name='index'),
+    path(r'', views.Index.as_view()),
     path(r'areas/', views.Area.as_view(), name='areas'),
     path(r'hospitallist/', views.HospitalList.as_view(), name='hospitallist'),
     path(r'hospital/', views.Hospital.as_view(), name='hospital')
