e38d3ecb9cab09a2d78334be53ff4f3c17681f52
meizongqi <398007089@qq.com>
2020-11-13 16:49:04 +0800

    试管婴儿

--- shiguanbaby/urls.py
+++ shiguanbaby/urls.py
@@ -5,6 +5,9 @@ from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
+    path(r"", views.Index.as_view(), name="index"),
     path(r"areas/", views.Areas.as_view(), name="areas"),
     path(r"articleslist/", views.ArticlesList.as_view(), name="articleslist"),
+    path(r"articles/", views.Articles.as_view(), name="article"),
+    path(r"hottopics/", views.HotTopics.as_view(), name="hottopics"),
 ]
