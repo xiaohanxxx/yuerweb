16b377e5964fb40aa52b913022cb562d78ae0ef9
wlmouse <m13260650015@163.com>
2020-11-17 15:18:43 +0800

    Revert "定义了2个公共模块"
    This reverts commit 22ed3b2d5833ad8d414ff3d029ca7406cd97ebd5.

--- yuerweb/views.py
+++ yuerweb/views.py
@@ -5,6 +5,7 @@ from webmanage import models
 def index(request):
     # 返回友情链接
     friend_link = [i for i in models.FriendLink.objects.all().order_by('idx').values('link_name','link','idx')]
+    print(friend_link)
     return render(request,'index.html',{"friendlink":friend_link})
 
 
