6082279e8eaca53cb7614170a9544b80c64844af
xiaohanxxx <47658907+xiaohanxxx@users.noreply.github.com>
2020-11-24 15:38:45 +0800

    个人中心指定

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from django.urls import re_path, path
 from users import views
 
 urlpatterns = [
-    path('', views.center),
     path('register', views.register),
     path('login', views.userlogin),
     path('changepwd', views.changepwd),
