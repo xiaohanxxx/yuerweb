608a0b4b4391c58537c062f0e664df08b438fc45
xiaohanxxx <47658907+xiaohanxxx@users.noreply.github.com>
2020-11-24 15:40:56 +0800

    个人中心指定

--- users/views.py
+++ users/views.py
@@ -31,6 +31,11 @@ def smsvif(request):
         }
         return HttpResponse(json.dumps(data), content_type="application/json")
 
+
+@login_required
+def center(request):
+    return render(request,'center.html')
+
 # 用户中心
 @login_required
 def centerMessage(request):
