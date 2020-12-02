406bf2eca198025e6141ec4463552d4165a31394
meizongqi <398007089@qq.com>
2020-12-02 17:51:34 +0800

    全文搜索

--- baike/search_indexes.py
+++ baike/search_indexes.py
@@ -7,7 +7,6 @@ from .models import *
 class ArticalIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
-    content = indexes.CharField(model_attr='content')
 
     def get_model(self):
         return Artical
